db917b1c7caa1db2572aca8c244ac91e5b8fcfc6
Joaquin Villanueva <74026054+JoaquinVF@users.noreply.github.com>
2021-06-07 00:50:01 -0300

    Update 4.0

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let textoEspañol = document.getElementById('esp');
-let textoIngles = document.getElementById('eng');
+const textoEspañol = document.getElementsByClassName('esp');
+const textoIngles = document.getElementsByClassName('eng');
+const selector = document.getElementById("selector");
+selector.addEventListener('change', (event) => {
+    if (event.target.value === "value2"){
+        langEs()
+    } else {
+        langEn()
+    }
+})
+
 
 function langEs(){
-    textoIngles.style.display = "none";
-    textoEspañol.style.display = "block";
+    for (let i = 0; i < textoEspañol.length; i++) {
+        textoIngles[i].style.display = "none";
+        textoEspañol[i].style.display = "block";
+    }
 }
 
 function langEn(){
-    textoIngles.style.display = "block";
-    textoEspañol.style.display = "none";
+    for (let i = 0; i < textoEspañol.length; i++) {
+        textoIngles[i].style.display = "block";
+        textoEspañol[i].style.display = "none";
+    }
 }
